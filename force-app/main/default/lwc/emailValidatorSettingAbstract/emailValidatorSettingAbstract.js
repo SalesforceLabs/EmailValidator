7140274e5f6a369a42a490b7b3dee9d1798f1f74
Giuseppe Cardace <gcardace@salesforce.com>
2023-01-06 13:03:42 +0100

    Backup 20220106_1303

--- force-app/main/default/lwc/emailValidatorSettingAbstract/emailValidatorSettingAbstract.js
+++ force-app/main/default/lwc/emailValidatorSettingAbstract/emailValidatorSettingAbstract.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire } from 'lwc';
 import getSettings from '@salesforce/apex/EmailValidatorSettingAbstract.getSettings'
 import saveSettings from '@salesforce/apex/EmailValidatorSettingAbstract.saveSettings'
+import settingExists from '@salesforce/apex/EmailValidatorSettingApp.settingExists'
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class EmailValidatorSettingAbstract extends LightningElement {
@@ -9,23 +10,54 @@ export default class EmailValidatorSettingAbstract extends LightningElement {
     val;
     password;
 
+    apikeyIsSet = false;
+    thresholdIsSet = false;
+
+
+    connectedCallback() {
+        this.checkSettings();
+    }
+
+    checkSettings() {
+        console.log('Checking which settings are available:');
+        settingExists({settingName: 'Abstract_API_Key'})
+        .then((result)=> {
+            this.apikeyIsSet = result;
+        })
+        .catch((error)=> {
+            console.log('ERROR WITH API KEY RETRIEVAL');
+            console.error(error);
+            this.apikeyIsSet = false;
+        });
+
+        settingExists({settingName: 'Abstract_Quality_Score_Threshold'})
+        .then((result)=> {
+            this.thresholdIsSet = result;
+        })
+        .catch((error)=> {
+            console.log('ERROR WITH QUALITY SCORE THRESHOLD RETRIEVAL');
+            console.error(error);
+            this.thresholdIsSet = false;
+        });
+    }
+
     @wire(getSettings)
     wiredSettings({ error, data }) {
         if (data) {
             if(data['Abstract_Quality_Score_Threshold']!=''){
-                this.val = +data['Abstract_Quality_Score_Threshold'];
+                this.val = +data['Abstract_Quality_Score_Threshold']*100;
             } else {
-                this.val = 0.5
+                this.val = 50;
             }
             this.error = undefined;
         } else if (error) {
-            this.val = 0.5
+            this.val = 50;
             this.error = error;
         }
     }
 
     handleSave(){
-        saveSettings({ threshold: this.val, password: this.password})
+        saveSettings({ threshold: this.val/100, password: this.password})
         .then((result)=>{
             this.password=null;
             this.template.querySelectorAll('lightning-input').forEach(element => {
@@ -36,7 +68,8 @@ export default class EmailValidatorSettingAbstract extends LightningElement {
                 message: 'Abstract configuration saved successfully.',
                 variant: 'success',
             });
-            this.dispatchEvent(evt);   
+            this.dispatchEvent(evt);
+            this.checkSettings(); 
         })
         .catch((error)=>{
             console.log(error);
